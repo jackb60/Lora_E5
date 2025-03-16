7c5f499a5be2fb117c98681a0ee4d2c763b5dfe3
jackb60 <40903548+jackb60@users.noreply.github.com>
2025-03-16 19:23:23 -0400

    Update Lora_E5.cpp

--- Lora_E5.cpp
+++ Lora_E5.cpp
@@ -6,10 +6,12 @@ Lora_E5::Lora_E5(Stream* ser, int rst) {
     _rst = rst;
 }
 
-bool Lora_E5::begin(unsigned long freq, int sf) {
+bool Lora_E5::begin(char* freq, int sf) {
+    _sf = sf;
+    _freq = freq;
     pinMode(_rst, OUTPUT);
     digitalWrite(_rst, 0);
-    delay(500);
+    delay(1000);
     _ser->print("AT\n");
     delay(200);
     _ser->print("AT+MODE=TEST\n");
@@ -20,6 +22,7 @@ bool Lora_E5::begin(unsigned long freq, int sf) {
     _ser->print(_sf);
     _ser->print(",125,8,8,22,ON,OFF,OFF\n");
     if(_ser->find("+TEST: RFCFG", 12)) {
+        delay(1);
         while(_ser->available()) {
             _ser->read();
             delay(1);
@@ -38,12 +41,13 @@ bool Lora_E5::reset() {
 
 bool Lora_E5::write(byte* data, byte len) {
     _ser->print("AT+TEST=TXLRPKT, \"");
-    for(byte i = 0; i < len; i++) {
+    for(int i = 0; i < len; i++) {
         _ser->print(*(data + i) < 0x10 ? "0" : "");
         _ser->print(*(data + i), HEX);
     }
     _ser->print("\"\n");
     if(_ser->find("+TEST: TX DONE", 14)) {
+        delay(1);
         while(_ser->available()) {
             _ser->read();
             delay(1);
@@ -56,6 +60,7 @@ bool Lora_E5::write(byte* data, byte len) {
 bool Lora_E5::listen() {
     _ser->write("AT+TEST=RXLRPKT\n");
     if(_ser->find("+TEST: RX", 9)) {
+        delay(1);
         while(_ser->available()) {
             _ser->read();
             delay(1);
@@ -91,10 +96,11 @@ byte Lora_E5::read() {
     
     
     String dtaStr = _ser->readStringUntil('"');
+    memset(buf, 0, 256);
     for (int i = 0; i < dtaStr.length(); i++) {
         byte data = dtaStr.charAt(i);
         data = (data < 0x41) ? data - 48 : data - 55;
-        buf[i / 2] = (i % 2 == 0) ? data : data << 4;
+        buf[i / 2] += (i % 2 == 1) ? data : data << 4;
     }
     bufLen = dtaStr.length() / 2;
 
